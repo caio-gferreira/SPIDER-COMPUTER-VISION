03eb5f1e1c980b722bc3b209360897cb304d6d90
caio-gferreira <caiogfferreira@gmail.com>
2024-02-17 13:41:45 -0300

    refactor open-cv proccessor: creating new methods for specific functionalities and organizing the code

--- src/main.py
+++ src/main.py
@@ -1,24 +1,17 @@
 import tensorflow as tf
-
 from tensorflow.keras import layers, models
+from services.open_cv_proccessor import OpenCVProcessor
 
-from helper.image_processor import ImageProcessor
-
-image_processor = ImageProcessor()
+image_processor = OpenCVProcessor()
 
 training_data_dir = 'src/images/treinamento'
 test_data_dir = 'src/images/teste'
 
-training_images, training_labels = image_processor.load_images(training_data_dir)
-testing_images, testing_labels = image_processor.load_images(test_data_dir)
-
-training_images = training_images[..., tf.newaxis].astype("float32")
-testing_images = testing_images[..., tf.newaxis].astype("float32")
+training_images, training_labels = image_processor.get_proccessed_images(training_data_dir)
+testing_images, testing_labels = image_processor.get_proccessed_images(test_data_dir)
 
 train_datasets = tf.data.Dataset.from_tensor_slices((training_images, training_labels)).shuffle(10000).batch(32)
 test_datasets = tf.data.Dataset.from_tensor_slices((testing_images, testing_labels)).batch(32)
-
-print("\nROTULOS:", training_labels)
 
 # Definir modelo CNN
 model = models.Sequential()
---
+++ src/services/open_cv_proccessor.py
@@ -0,0 +1,54 @@
+import cv2
+import os
+import numpy as np
+import tensorflow as tf
+
+class OpenCVProcessor:
+    def get_proccessed_images(self, folder):
+        images, labels = self.load_images(folder)
+
+        images_proccessed = self.process_images(images)
+        labels_proccessed = self.process_labels(labels)
+
+        return images_proccessed, labels_proccessed
+
+
+    def load_images(self, folder):
+        list_images_spiders = []
+        list_labels_spiders_images = []
+    
+        for filename in os.listdir(folder):
+            label_spider_image = filename.split('.')[0]
+            image_spider = cv2.imread(os.path.join(folder, filename))  
+
+            if image_spider is not None:
+                list_images_spiders.append(image_spider)
+                list_labels_spiders_images.append(label_spider_image)
+    
+        return list_images_spiders, list_labels_spiders_images
+
+    def process_images(self, images_list):
+        images_formateds_list = []
+
+        for image in images_list:
+           formated_image = self.format_image(image)
+           images_formateds_list.append(formated_image)
+        
+        images_proccesseds_list = np.array(images_formateds_list)
+        images_proccesseds_list = images_proccesseds_list[..., tf.newaxis].astype("float32")
+
+        return images_proccesseds_list
+    
+    def process_labels(self, labels_list):
+        label_to_index = {label: i for i, label in enumerate(np.unique(labels_list))}
+
+        labels_proccessed_list = np.array([label_to_index[label] for label in labels_list])
+
+        return labels_proccessed_list
+    
+    def format_image(self, image):
+        resized_image = cv2.resize(image, (28, 28))
+        recolor_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+        formated_image = recolor_image
+
+        return formated_image
